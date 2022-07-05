4d4b9f8fe62829f2c148f8483d3f4d861f5d5fc2
Alejandro Calva <101780546+JuniorCalva40@users.noreply.github.com>
2022-07-04 19:19:12 -0500

    Add files via upload

---
+++ Tareas/Tarea 1.1/Funcion Javascript/javascript.js
@@ -0,0 +1,6 @@
+const nombre = "Juan David";
+const lastname = "Castro Gallego";
+const completeName = nombre + lastname;
+const nickname = "juandc";
+console.log("Mi nombre es " + completeName + , pero prefiero
+que me digas" + nickname + ".");
---
+++ Tareas/Tarea 1.2/Ejercicios While/js.js
@@ -0,0 +1,18 @@
+    //Ejercicios ciclos while
+     
+    
+     let i = 0;
+     while(i < 5){
+     console.log("El valor de i es: " + i);    
+     i++;
+     }
+
+     let cont = 10;
+     while(cont >= 2){
+     console.log("El valor de cont es: " + cont);    
+     cont--;
+     }
+
+
+
+  
