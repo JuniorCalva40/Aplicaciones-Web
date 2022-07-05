2600a3237ccaf7ade9998cb7f816985e2aaac822
Alejandro Calva <101780546+JuniorCalva40@users.noreply.github.com>
2022-07-04 19:36:11 -0500

    Delete Tareas directory

--- Tareas/Tarea 1.1/Funcion Javascript/javascript.js
+++
@@ -1,6 +0,0 @@
-const nombre = "Juan David";
-const lastname = "Castro Gallego";
-const completeName = nombre + lastname;
-const nickname = "juandc";
-console.log("Mi nombre es " + completeName + , pero prefiero
-que me digas" + nickname + ".");
