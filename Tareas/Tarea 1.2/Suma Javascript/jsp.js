02c96cb95f03881b9732f2604d84458d7bb65595
Alejandro Calva <101780546+JuniorCalva40@users.noreply.github.com>
2022-07-04 19:37:09 -0500

    Add files via upload

---
+++ Tareas/Tarea 1.2/Suma Javascript/jsp.js
@@ -0,0 +1,9 @@
+//Ejercicio Suma JavaScript
+
+let pregunta= prompt("Hey! ¿Cuánto es 2 + 2?");
+
+if ( pregunta == 4){
+    document.write("<h2> 🤓 Felicitaciones eres un Genio </h2>")
+} else{
+    document.write("<h2>  😆 Intenta de Nuevo </h2>")
+}
