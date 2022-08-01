5b314150be46615953bc278c4b07ef7f00578037
Alejandro Calva <101780546+JuniorCalva40@users.noreply.github.com>
2022-08-01 18:48:23 -0500

    Update exercise2.js

--- Tareas/Tareas semana 6/exercise2.js
+++ Tareas/Tareas semana 6/exercise2.js
@@ -1,4 +1,9 @@
 // AÑO BISIESTO
+/*Construir un programa que pida un año y luego determine si el
+año es bisiesto o no. Tenga en cuenta que los años bisiestos son
+los números múltiplos de 4, pero que no son múltiplos de 100 y si
+son múltiplos de 100 y múltiplos de 400 sí son bisiestos.*/
+
 let year =parseInt(prompt("Ingrese el año para verificar si es bisiesto:"));
    
     if((year % 4 ==0)&&(year %100 !==0 || year%400==0)){
